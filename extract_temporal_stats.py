3d4455318c543454faffe01119e983efd8b1ce66
agonmer <agostino.meroni@gmail.com>
2023-02-28 11:14:00 +0100

    KD and SW computations added

--- extract_temporal_stats.py
+++ extract_temporal_stats.py
@@ -39,6 +39,10 @@ instant_end = np.datetime64(str_end)
 # Set the half width of the box over which to take the average.
 dlat = 0.1
 dlon = 0.1
+dlonn = 0.2
+dlatt = 0.2
+dlonnn = 0.5
+dlattt = 0.5
 
 allstats_db = np.array(['LONG','LAT','Water depth','Stations/Facies',
                         'SSTmin_K','SSTavg_K','SSTmax_K',
@@ -101,8 +105,6 @@ for ss in range(len(df_loc['LONG'])): # np.arange(498,500): # range(len(df_loc['
             sst0 = ds_sst['analysed_sst'][mm].sel(lon=slice(lon0-dlon,lon0+dlon),
                                                   lat=slice(lat0-dlat,lat0+dlat)).mean(dim=['lon','lat'],skipna=True).values
             if np.isnan(sst0): # Extend the area over which the sst is read.
-                dlonn = 0.2
-                dlatt = 0.2
                 sst0 = ds_sst['analysed_sst'][mm].sel(lon=slice(lon0-dlonn,lon0+dlonn),
                                                       lat=slice(lat0-dlatt,lat0+dlatt)).mean(dim=['lon','lat'],skipna=True).values
                 the_area_has_been_extended_sst = True
@@ -119,8 +121,6 @@ for ss in range(len(df_loc['LONG'])): # np.arange(498,500): # range(len(df_loc['
             chla0 = ds_chla['chlor_a'][0].sel(lon=slice(lon0-dlon,lon0+dlon),
                                               lat=slice(lat0+dlat,lat0-dlat)).mean(dim=['lon','lat'],skipna=True).values
             if np.isnan(chla0): # Extend the area over which the sst is read.
-                dlonn = 0.2
-                dlatt = 0.2
                 chla0 = ds_chla['chlor_a'][0].sel(lon=slice(lon0-dlonn,lon0+dlonn),
                                                  lat=slice(lat0-dlatt,lat0+dlatt)).mean(dim=['lon','lat'],skipna=True).values
                 the_area_has_been_extended_chla = True
@@ -173,3 +173,156 @@ for ss in range(len(df_loc['LONG'])): # np.arange(498,500): # range(len(df_loc['
 pd.DataFrame(allstats_db).to_csv('stats_prova.csv')
 pd.DataFrame(list_of_nans_sst).to_csv('list_sites_larger_area_sst.csv')
 pd.DataFrame(list_of_nans_chla).to_csv('list_sites_larger_area_chla.csv')
+
+# Analysis on KD and SW. SW is the shortwave flux at the surface and SWz is at depth, computed as
+# SWz = SW*exp(-KD*wd0)
+
+allstats_d2 = np.array(['LONG','LAT','Water depth','Stations/Facies',
+                        'KDmin_m-1','KDavg_m-1','KDmax_m-1',
+                        'KDJan_m-1','KDFeb_m-1','KDMar_m-1','KDApr_m-1','KDMay_m-1','KDJun_m-1',
+                        'KDJul_m-1','KDAug_m-1','KDSep_m-1','KDOct_m-1','KDNov_m-1','KDDec_m-1',
+                        'SWmin_W_m-2','SWavg_W_m-2','SWmax_W_m-2',
+                        'SWJan_W_m-2','SWFeb_W_m-2','SWMar_W_m-2','SWApr_W_m-2','SWMay_W_m-2','SWJun_W_m-2',
+                        'SWJul_W_m-2','SWAug_W_m-2','SWSep_W_m-2','SWOct_W_m-2','SWNov_W_m-2','SWDec_W_m-2',
+                        'SWmin_W_m-2','SWavg_W_m-2','SWmax_W_m-2',
+                        'SWJan_W_m-2','SWFeb_W_m-2','SWMar_W_m-2','SWApr_W_m-2','SWMay_W_m-2','SWJun_W_m-2',
+                        'SWJul_W_m-2','SWAug_W_m-2','SWSep_W_m-2','SWOct_W_m-2','SWNov_W_m-2','SWDec_W_m-2'
+                       ])
+
+list_of_nans_kd = []
+
+for ss in np.arange(700,701): #range(len(df_loc['LONG'])): # Loop on the sites.
+    # Select the point.
+    lon0 = df_loc['LONG'][ss]
+    lat0 = df_loc['LAT'][ss]
+    wd0 = df_loc['Water depth'][ss]
+    sf0 = str(df_loc['Stations/Facies'][ss])
+    
+    print('--------------------------------------------')
+    print(sf0 + ' at ' + str(wd0) +' m')
+    print(str(ss) +' out of '+ str(len(df_loc['LONG'])))
+    print('--------------------------------------------')
+
+    # Time loop
+    instant = instant_start
+    it_s_the_first_instant = True
+    time_series = []
+    kd_series = []
+    sw_series = []
+    swz_series = []
+    kd_seasonal = np.zeros(12) # To compute the seasonal cycle
+    kd_seasonal_count = np.zeros(12)
+    sw_seasonal = np.zeros(12)
+    sw_seasonal_count = np.zeros(12)
+    swz_seasonal = np.zeros(12)
+    swz_seasonal_count = np.zeros(12)
+    nyears = 0 # Number of years.
+
+    the_area_has_been_extended_kd = False
+    the_area_has_been_extended_sw = False
+
+    while instant <= instant_end:
+        print(instant)
+        pd_instant = pd.to_datetime(instant)
+        year_oi = str(pd_instant.year).zfill(4)
+
+        # Set the path of the monthly Chla data for the current year.
+        path_kd_oi = path2kd + '/' +  year_oi + '/'
+        path_sw_oi = path2sw + '/' +  year_oi + '/'
+
+        for mm in range(12):
+            # Define the monthly time step in the time_series.
+            month_oi = str(mm+1).zfill(2)
+            time_series.extend([year_oi + '-' + month_oi])
+                        
+            # Read the KD data.
+            filename_kd = 'ESACCI-OC-L3S-K_490-MERGED-1M_MONTHLY_4km_GEO_PML_KD490_Lee-'+year_oi+month_oi+'-fv5.0.nc'
+            ds_kd = xr.open_dataset(path_kd_oi + filename_kd)
+
+            kd0 = ds_kd['kd_490'][0].sel(lon=slice(lon0-dlon,lon0+dlon),
+                                         lat=slice(lat0+dlat,lat0-dlat)).mean(dim=['lon','lat'],skipna=True).values
+            if np.isnan(kd0): # Extend the area over which the sst is read.
+                dlonn = 0.2
+                dlatt = 0.2
+                kd0 = ds_kd['kd_490'][0].sel(lon=slice(lon0-dlonn,lon0+dlonn),
+                                             lat=slice(lat0-dlatt,lat0+dlatt)).mean(dim=['lon','lat'],skipna=True).values
+                the_area_has_been_extended_kd = True
+
+            kd_series.extend([kd0.item()])
+            kd_seasonal[mm] += kd0
+            kd_seasonal_count[mm] += 1
+
+            # Read the SW data. 
+            # There should not be NaN, as the data are gap-free and are defined over land, as well.
+            # Both fluxes are positive and the upward is smaller than the downward.
+            filename_sw = year_oi+month_oi+'-ESACCI-L3C_CLOUD-CLD_PRODUCTS-AVHRR_METOPA-fv3.0.nc'
+            ds_sw = xr.open_dataset(path_sw_oi + filename_sw)
+
+            swup0 = ds_sw['boa_swup'][0].sel(lon=slice(lon0-dlonnn,lon0+dlonnn),
+                                             lat=slice(lat0-dlattt,lat0+dlattt)).mean(dim=['lon','lat'],skipna=True).values
+            swdn0 = ds_sw['boa_swdn'][0].sel(lon=slice(lon0-dlonnn,lon0+dlonnn),
+                                             lat=slice(lat0-dlattt,lat0+dlattt)).mean(dim=['lon','lat'],skipna=True).values
+            #print('swup',swup0,'swdn',swdn0)
+            
+            # Compute the net shortwave flux.
+            sw0 = swdn0-swup0
+                
+            sw_series.extend([sw0.item()])
+            sw_seasonal[mm] += sw0
+            sw_seasonal_count[mm] += 1
+            
+            # Approximately compute the net shortwave flux at depth.
+            swz0 = sw0*np.exp(-kd0*wd0)
+            
+            swz_series.extend([swz0.item()])
+            swz_seasonal[mm] += swz0
+            swz_seasonal_count[mm] += 1
+            
+            if it_s_the_first_instant:
+                # Check the position of the point on a map.
+                crs = ccrs.PlateCarree()
+                lon_kd = ds_kd['lon'].values
+                lat_kd = ds_kd['lat'].values
+                kd_map = ds_kd['kd_490'].values
+                lon_sw = ds_sw['lon'].values
+                lat_sw = ds_sw['lat'].values
+                swup_map = ds_sw['boa_swup'].values
+                swdn_map = ds_sw['boa_swdn'].values
+
+                it_s_the_first_instant = False
+                
+        instant = np.datetime64(str(int(year_oi)+1)+'-01-01') #+= np.timedelta64(1,'Y')
+        nyears += 1
+        
+    if the_area_has_been_extended_kd:
+        list_of_nans_kd.append(sf0+'_at_'+str(wd0))
+        print('kd:',list_of_nans_kd)    
+
+    # Compute the statistics of interest for SST and Chla.
+    kd_t = np.array(kd_series)
+    kd_min = np.nanmin(kd_t)
+    kd_max = np.nanmax(kd_t)
+    kd_avg = np.nanmean(kd_t)
+    kd_seasonal_cycle = kd_seasonal/kd_seasonal_count 
+    kd_stats = np.concatenate((np.array([lon0,lat0,wd0,sf0,kd_min,kd_avg,kd_max]),kd_seasonal_cycle))
+    
+    sw_t = np.array(sw_series)
+    sw_min = np.nanmin(sw_t)
+    sw_max = np.nanmax(sw_t)
+    sw_avg = np.nanmean(sw_t)
+    sw_seasonal_cycle = sw_seasonal/sw_seasonal_count 
+    sw_stats = np.concatenate((np.array([sw_min,sw_avg,sw_max]),sw_seasonal_cycle))
+
+    swz_t = np.array(swz_series)
+    swz_min = np.nanmin(swz_t)
+    swz_max = np.nanmax(swz_t)
+    swz_avg = np.nanmean(swz_t)
+    swz_seasonal_cycle = swz_seasonal/swz_seasonal_count 
+    swz_stats = np.concatenate((np.array([swz_min,swz_avg,swz_max]),swz_seasonal_cycle))
+
+    allstats_d2 = np.vstack([allstats_d2,np.concatenate([kd_stats,sw_stats,swz_stats])])
+    
+    #print(allstats_db)
+    
+pd.DataFrame(allstats_d2).to_csv('stats_prov2.csv')
+pd.DataFrame(list_of_nans_kd).to_csv('list_sites_larger_area_kd.csv')
